fb96ac8b8ab43e01544909edf9cb65bd39feeeae
build_test <wkisielewicz@criticue.com>
2016-04-27 13:43:00 +0200

    nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users
   resources :static_pages
-  # resources :tips
-  # resources :articles
+ 
 
 resources :tips do
   resources :articles
