398f90d790184309585e82c9241b8f76fd14b6df
wkisielewicz <wkisielewicz@criticue.com>
2016-10-01 17:51:03 +0200

    Removed comments.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users
   resources :static_pages
-  resources :tips
   resources :articles
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-# resources :tips do
-#   resources :articles
-#   end
 end
